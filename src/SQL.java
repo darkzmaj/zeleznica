100e6a081d0f501b7fe6d17ae64087b2d13dcded
Jovan Jovanovic <zmaj93@live.com>
2018-10-01 08:24:05 +0200

    Gotova aplikacija, spremno za prezentaciju 3

---
+++ src/SQL.java
@@ -0,0 +1,27 @@
+
+public class SQL {
+    /*
+    
+    
+CREATE TABLE `komentari` (
+  `toneriid` int(11) NOT NULL,
+  `komentariid` int(11) NOT NULL AUTO_INCREMENT,
+  `Komentar` varchar(45) DEFAULT NULL,
+  `DatumIzmene` date DEFAULT NULL,
+  PRIMARY KEY (`komentariid`,`toneriid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `toneri` (
+  `toneriid` int(11) NOT NULL AUTO_INCREMENT,
+  `proizvodjac` varchar(45) DEFAULT '/',
+  `tip` varchar(45) DEFAULT '/',
+  `proizvod` varchar(45) DEFAULT '/',
+  `oznaka` varchar(45) DEFAULT '/',
+  `stampaci` varchar(255) DEFAULT '/',
+  `kolicina` int(11) DEFAULT '0',
+  PRIMARY KEY (`toneriid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;    
+     
+    
+     */
+}
